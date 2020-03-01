8ced46247a879814235085d9abcf81dcfbd95536
felixisto <felixisto@outlook.com>
2020-03-01 21:27:18 +0200

    Bumped version to 1.2 (3)

--- NotABadPlayer/Constants/Text.swift
+++ NotABadPlayer/Constants/Text.swift
@@ -73,7 +73,7 @@ enum Text: String {
     case SettingsQPlayerNext = "QPlayer Next"
     case SettingsLockPlayerPrevious = "LPlayer Previous"
     case SettingsLockPlayerNext = "LPlayer Next"
-    case SettingsAbout = "Version 1.1 (2)\nReleased 2019.12.7\nProgrammed, designed & tested by Kristiyan Butev"
+    case SettingsAbout = "Version 1.2 (3)\nReleased 2020.03.1\nProgrammed, designed & tested by Kristiyan Butev"
     
     case Error = "Error"
     case ErrorUnknown = "Unknown error occured"
